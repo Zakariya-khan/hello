098bee218ad17fcf7988203fe76c28ab618d1a13
Zakariya-khan <zakariakhan123.zk@gmail.com>
2021-03-02 12:24:26 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,9 +7,8 @@ function App() {
   return (
     <div className="App">
       <h1> Trending Products </h1>
-      <Products   product1=" Water Monitoring Device"   />
-      <Products   product2=" Face Detector"   />
-      <Products   product3=" Heat Sensor"   />
+      <Products   product1=" Water Monitoring Device" product2=" Face Detector" product3=" Heat Sensor"  />
+     
     </div>
   );
 }
