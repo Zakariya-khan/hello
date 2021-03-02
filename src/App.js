7b5f250be1c947749fd85d8c90049f08e0af5cb8
Zakariya-khan <zakariakhan123.zk@gmail.com>
2021-03-02 17:22:15 +0500

    components added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
-import logo from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Products from './IOTSOL.js';
+import {Counter} from './Message.js';
 
 
-function App() {
+export default function App() {
+  let [count, setcount] = useState(1);
+  let [ isMOrning , setMorning] = useState(true)
   return (
-    <div className="App">
+    <div className={` box ${isMorning ? 'daylight' : ''}` }>
+      <h1> Day time = {isMorning ? 'Morning' : 'Night'}</h1>
       <h1> Trending Products </h1>
-      <Products   product1=" Water Monitoring Device" product2=" Face Detector" product3=" Heat Sensor"  />
-     
+      <Products product1=" Water Monitoring Device" product2=" Face Detector" product3=" Heat Sensor" />
+      {/* <h1>{count}</h1> */}
+      <Counter  counter = {count}/>
+      <button onClick={
+        () => setcount(count + 1)
+      }> Update Counter </button>
     </div>
   );
 }
 
-export default App;
+
